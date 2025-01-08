d6a1bbb83267beeb8d3a92c1392fc6f317499d51
LRQUITOR <lrquitor@alumno.upv.es@ldsic-vdi12.upvnet.upv.es>
2025-01-08 22:49:59 +0100

    agregando cosas

--- Point2D.cpp
+++ Point2D.cpp
@@ -1,21 +1,28 @@
 #include "Point2D.h"
 
-Point2D::Point2D(double x=0, double y=0){
-	this->x=x;
-	this->y=y;
-}
+Point2D::Point2D(double x,double y): x(x) , y(y) {}
 double Point2D::distance(const Point2D &a,const Point2D &b){
-	double j=(b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y);
-	double dist=sqrt(j);
-	return dist;
+
+    double d=sqrt(pow((a.x)-(b.x),2)+pow((a.y)-(b.y),2));
+    return d; 
 }
 bool operator==(const Point2D &a, const Point2D &b){
-	return (a.x==b.x && a.y==b.y);
+
+    return a.x == b.x && a.y == b.y;
+
 }
 bool operator!=(const Point2D &a, const Point2D &b){
-	return !(a==b);
+   
+    return !(a==b);	
+
+
 }
-std::ostream &operator<<(std::ostream &out, const Point2D &p){
-	out<<"("<<p.x <<", "<<p.y<<")";
+std::ostream& operator<<(std::ostream &out, const Point2D &p){
+
+
+	out<<"("<<p.x<<","<<p.y<<") ";
 	return out;
+
 }
+
+
--- Point2D.h
+++ Point2D.h
@@ -1,17 +1,21 @@
 #ifndef POINT2D_H
 #define POINT2D_H
 
+#include <iostream>
 #include <cmath>
-#include <ostream>
-class Point2D
-{
- public:
-    double x,y;
-    Point2D(double x, double y);
-    friend bool operator==(const Point2D &a,const Point2D &b);
-    friend bool operator!=(const Point2D &a,const Point2D &b);
-    friend std::ostream &operator<<(std::ostream &out, const Point2D &p);
-    static double distance (const Point2D &a,const Point2D &b);
+class Point2D{
+
+	public:
+		double x,y;
+		Point2D(double x=0,double y=0);
+		static double distance(const Point2D &a,const Point2D &b);
+		friend bool operator==(const Point2D &a, const Point2D &b);
+		friend bool operator!=(const Point2D &a, const Point2D &b);
+		friend std::ostream& operator<<(std::ostream &out, const Point2D &p);
+
+
+
 
 };
- #endif
+
+#endif
